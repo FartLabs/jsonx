7cd46a678e2ed47fa380a6c9b473db181f2b34b8
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-02-25 09:31:20 -0800

    update local assets tests

--- lib/std/local_assets/components_test.tsx
+++ lib/std/local_assets/components_test.tsx
@@ -1,22 +1,19 @@
 import { assertEquals } from "std/assert/mod.ts";
-import { Assets } from "jsonx/std/assets/mod.ts";
+import { AssetKind, EncodingType } from "jsonx/std/assets/mod.ts";
 import { LocalAsset } from "./components.tsx";
 
 Deno.test("Local assets read from file system successfully", () => {
   const actual = (
-    <Assets>
-      <LocalAsset
-        path="src/lib/components/hello.svelte"
-        src={new URL(import.meta.resolve("./testdata/hello.svelte"))}
-      />
-    </Assets>
+    <LocalAsset
+      path="src/lib/components/hello.svelte"
+      src={new URL(import.meta.resolve("./testdata/hello.svelte"))}
+    />
   );
-
   const expected = {
     assets: {
       "src/lib/components/hello.svelte": {
-        kind: "file",
-        encoding: "base64",
+        kind: AssetKind.FILE,
+        encoding: EncodingType.BASE64,
         content:
           "PHNjcmlwdCBsYW5nPSJ0cyI+DQogIGV4cG9ydCBsZXQgbmFtZSA9ICd3b3JsZCc7DQo8L3NjcmlwdD4NCg0KPGgxPkhlbGxvIHtuYW1lfSE8L2gxPg0K",
       },
