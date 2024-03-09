9bbc71adf2138df908a4717bef70104198c18569
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-09 09:07:05 -0800

    add `lib/github_workflows` library

--- gen.ts
+++ gen.ts
@@ -1,2 +1,2 @@
-//deno:generate deno run --allow-write .github/workflows/check.tsx .github/workflows/check.yaml
-//deno:generate deno run --allow-write .github/workflows/publish.tsx .github/workflows/publish.yaml
+//deno:generate deno run --allow-write lib/github_workflows/check.tsx .github/workflows/check.yaml
+//deno:generate deno run --allow-write lib/github_workflows/publish.tsx .github/workflows/publish.yaml
