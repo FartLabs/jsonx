971c0a03b6157cfa4269ee4b4cf50e9cac474cff
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-13 21:14:50 -0700

    Revert "monaco editor also no worky"
    This reverts commit d8f548eb7605de339ba0b39dbb88326213aa76b4.

--- docs/static/lib/playground/editor.js
+++ docs/static/lib/playground/editor.js
@@ -1,38 +1,8 @@
-import * as monaco from "https://esm.sh/monaco-editor@0.47.0";
-import editorWorker from "https://esm.sh/monaco-editor@0.47.0/esm/vs/editor/editor.worker?worker";
-import jsonWorker from "https://esm.sh/monaco-editor@0.47.0/esm/vs/language/json/json.worker?worker";
-import cssWorker from "https://esm.sh/monaco-editor@0.47.0/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "https://esm.sh/monaco-editor@0.47.0/esm/vs/language/html/html.worker?worker";
-import tsWorker from "https://esm.sh/monaco-editor@0.47.0/esm/vs/language/typescript/ts.worker?worker";
-
-self.MonacoEnvironment = {
-  getWorker(_, label) {
-    if (label === "json") {
-      return new jsonWorker();
-    }
-
-    if (label === "css" || label === "scss" || label === "less") {
-      return new cssWorker();
-    }
-
-    if (label === "html" || label === "handlebars" || label === "razor") {
-      return new htmlWorker();
-    }
-
-    if (label === "typescript" || label === "javascript") {
-      return new tsWorker();
-    }
-
-    return new editorWorker();
-  },
-};
+import ace from "https://esm.sh/ace-builds@1.32.7";
 
 let EDITOR;
 
 export function createEditor(options) {
-  EDITOR = monaco.editor.create(options.target, {
-    value: options.code,
-    language: "javascript", // "tsx"
-  });
+  EDITOR = ace.edit(options.target);
   console.log({ EDITOR });
 }
