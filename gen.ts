a056f0ffc34e3259c9a95c8e5775ee6efa830ea0
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-11-15 02:18:21 -0800

    Replace `deno generate` with `deno task generate`

--- gen.ts
+++
@@ -1,2 +0,0 @@
-//deno:generate deno run --allow-write .github/workflows/check.tsx .github/workflows/check.yaml
-//deno:generate deno run --allow-write .github/workflows/publish.tsx .github/workflows/publish.yaml
